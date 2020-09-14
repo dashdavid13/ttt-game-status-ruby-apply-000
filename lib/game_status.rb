2bd8239917480cfd841c731c1ec82e3c4b9e3c75
David Medina <davidamedina619@gmail.com>
2020-09-14 01:57:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,7 +61,16 @@ end
    
  end
  
- def winner(board)
-   
-   
- end
+ def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
