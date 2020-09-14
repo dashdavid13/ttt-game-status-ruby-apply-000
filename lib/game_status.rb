c856d9eee9e92f23a2a8c3e662782afcf3aad6d3
David Medina <davidamedina619@gmail.com>
2020-09-14 01:47:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,9 @@ end
         position_1 = board[win_index_1]
         position_2 = board[win_index_2] 
         position_3 = board[win_index_3] 
-        if position_1
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination 
+      elsif position 
   end
   end
  
